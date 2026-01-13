2060e860248fd15c45a38f2eac94208b3524f63f
Raagam Parmar <raagamparmar@gmail.com>
2026-01-13 14:00:11 +0530

    Added module for NFA.

--- lib/automata.ml
+++ lib/automata.ml
@@ -11,9 +11,9 @@ end
 
 module Make (A : SymbolType) =
 struct
-  module AS = Set.Make(A)
+  module AS = Setutils.Make(A)
   module Q  = Int
-  module QS = Set.Make(Q)
+  module QS = Setutils.Make(Q)
 
   type chr = A.t
   type str = chr list
@@ -36,5 +36,31 @@ struct
     let accepts dfa (str : str) =
       let final = step' dfa dfa.init str in
       QS.mem final dfa.final
+  end
+
+  module NFA =
+  struct
+    type ta =
+      | Sym of A.t
+      | Eps
+
+    type t =
+      { init  : Q.t  ;
+        final : QS.t ;
+        step  : Q.t -> ta -> QS.t
+      }
+
+    let step nfa = nfa.step
+
+    let eps_closure nfa (s : QS.t) =
+      let rec go t =
+        let t' = QS.map_union (fun s -> nfa.step s Eps) t in
+        let dif = QS.diff t' t in
+        if QS.is_empty dif
+        then t
+        else go t'
+      in
+      go s
+
   end
 end
