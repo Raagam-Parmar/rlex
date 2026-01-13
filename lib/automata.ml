720b69ca13a5c5b31af45b30391bd26f4fb8dc85
Raagam Parmar <raagamparmar@gmail.com>
2026-01-13 10:44:46 +0530

    Added DFA module and dfa type.

---
+++ lib/automata.ml
@@ -0,0 +1,34 @@
+module type SymbolType =
+sig
+  type t
+  val compare: t -> t -> int
+end
+
+module type DFAType =
+sig
+  type t
+end
+
+module Make (A : SymbolType) =
+struct
+  module AS = Set.Make(A)
+  module Q  = Int
+  module QS = Set.Make(Q)
+
+  type chr = A.t
+  type str = chr list
+
+  module DFA =
+  struct
+    type t =
+      { states : QS.t;
+        init   : Q.t;
+        alpha  : AS.t;
+        final  : QS.t;
+        step   : Q.t -> A.t -> Q.t
+      }
+
+    let step' dfa (q : Q.t) (str : str) =
+      List.fold_left dfa.step q str
+  end
+end
