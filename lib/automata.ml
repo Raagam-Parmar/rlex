4bffa023e689dab7db6e6f3121f368fc9905225c
Raagam Parmar <raagamparmar@gmail.com>
2026-01-13 10:56:09 +0530

    Added extended step function and acceptance checker for DFA.

--- lib/automata.ml
+++ lib/automata.ml
@@ -21,14 +21,20 @@ struct
   module DFA =
   struct
     type t =
-      { states : QS.t;
-        init   : Q.t;
-        alpha  : AS.t;
-        final  : QS.t;
+      { states : QS.t ;
+        init   : Q.t  ;
+        alpha  : AS.t ;
+        final  : QS.t ;
         step   : Q.t -> A.t -> Q.t
       }
 
+    let step dfa = dfa.step
+
     let step' dfa (q : Q.t) (str : str) =
       List.fold_left dfa.step q str
+
+    let accepts dfa (str : str) =
+      let final = step' dfa dfa.init str in
+      QS.mem final dfa.final
   end
 end
