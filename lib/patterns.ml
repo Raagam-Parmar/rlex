a45a81b1a6e689a701810fbdf933f366cc4d37eb
Raagam Parmar <raagamparmar@gmail.com>
2026-01-14 21:23:38 +0530

    Added helper functions to build complex patterns.

--- lib/patterns.ml
+++ lib/patterns.ml
@@ -7,3 +7,32 @@ type pattern =
   | PConcat of pattern * pattern
   | PStar   of pattern
   | PCompl  of pattern
+
+let null = PNull
+let eps = PEps
+let eof = PEof
+let chr c = PChr c
+let union p1 p2 = PUnion (p1, p2)
+let concat p1 p2 = PConcat (p1, p2)
+let star p = PStar p
+let compl p = PCompl p
+let inter p1 p2 = union (compl p1) (compl p2)
+let diff p1 p2 = inter p1 (compl p2)
+
+let chr_range c1 c2 =
+  let len = Int.abs (Char.code c1 - Char.code c2) in
+  List.init len Char.chr
+  |> List.map chr
+  |> List.fold_left union null
+
+let chr_set ccl =
+  ccl
+  |> List.map (fun (c1, c2) -> chr_range c1 c2)
+  |> List.fold_left union null
+
+let str =
+  String.fold_left (fun p c -> union p (chr c)) null
+
+let plus p = concat p (star p)
+
+let opt p = union p eps
