7077ec6492e647df445c815a27ac4c67b05dfdc1
Raagam Parmar <raagamparmar@gmail.com>
2026-01-14 22:37:44 +0530

    Added NFA to match empty string and a single character.

--- lib/matcher.ml
+++ lib/matcher.ml
@@ -46,3 +46,39 @@ let accept_null =
   | Error _ ->
     raise (UnepectedError "Compile.accept_null : unexpected faliure")
   | Ok n -> n
+
+let accept_eps =
+  let n =
+    NfaChar.mk_nfa
+      ~states:(SetInt.singleton 0)
+      ~init:0
+      ~alpha:all_symbols
+      ~final:(SetInt.singleton 0)
+      ~step:(fun _ _ -> SetInt.empty)
+  in
+  match n with
+  | Error _ ->
+    raise (UnepectedError "Compile.accept_eps : unexpected faliure")
+  | Ok n -> n
+
+let accept_chr c =
+  let n =
+    NfaChar.mk_nfa
+      ~states:(SetInt.of_list [0; 1])
+      ~init:0
+      ~alpha:all_symbols
+      ~final:(SetInt.singleton 1)
+      ~step:
+        (fun q s ->
+           if q = 0 then
+             if NfaChar.cmp_ts s c = 0
+             then SetInt.singleton 1
+             else SetInt.empty
+           else
+             SetInt.empty
+        )
+  in
+  match n with
+  | Error _ ->
+    raise (UnepectedError "Compile.accept_eps : unexpected faliure")
+  | Ok n -> n
