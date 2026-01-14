91083c2761ecdc05b265b10adfbe4798b6fa5db6
Raagam Parmar <raagamparmar@gmail.com>
2026-01-14 16:05:34 +0530

    Cleaned up DFA interface and code.

--- lib/dfa.ml
+++ lib/dfa.ml
@@ -1,22 +1,58 @@
-module type SymbolType =
+module type ALPHABET_TYPE =
 sig
   type t
-  val compare: t -> t -> int
+  val compare : t -> t -> int
 end
 
-module type DFAType =
+module type STATE_TYPE =
 sig
   type t
+  val compare : t -> t -> int
 end
 
-module Make (A : SymbolType) =
+module type DFA_TYPE =
+sig
+  type t
+
+  type s
+  type q
+
+  type err =
+    | InvalidChar
+    | EmptyStates
+    | EmptyAlpha
+    | InvalidInit
+    | InvalidFinal
+    | InvalidStep
+
+  val mk_dfa :
+    states   : q list
+    -> init  : q
+    -> alpha : s list
+    -> final : q list
+    -> step  : (q -> s -> q)
+    -> (t, err) Result.t
+
+  val states  : t -> q list
+  val init    : t -> q
+  val alpha   : t -> s list
+  val final   : t -> q list
+  val step    : t -> q -> s -> (q, err) Result.t
+  val step'   : t -> q -> s list -> (q, err) Result.t
+  val accepts : t -> s list -> (bool, err) Result.t
+end
+
+module Make
+    (A : ALPHABET_TYPE)
+    (Q : STATE_TYPE)
+  : DFA_TYPE =
 struct
   module AS = Setutils.Make(A)
-  module Q  = Int
   module QS = Setutils.Make(Q)
 
-  type chr = A.t
-  type str = chr list
+  type s = A.t
+  type q = Q.t
+  type str = s list
 
   type t =
     { states : QS.t ;
@@ -27,66 +63,101 @@ struct
     }
 
   type err =
-    | InvalidString of str * AS.t
+    | InvalidChar
     | EmptyStates
     | EmptyAlpha
-    | InvalidInit of Q.t * QS.t
-    | InvalidFinal of QS.t * QS.t
-    | InvalidStep of Q.t * A.t * Q.t * QS.t
+    | InvalidInit
+    | InvalidFinal
+    | InvalidStep
 
-  let step dfa = dfa.step
 
-  let step' dfa (q : Q.t) (str : str) =
-    List.fold_left dfa.step q str
+  let states dfa = QS.to_list dfa.states
+  let init dfa = dfa.init
+  let alpha dfa = AS.to_list dfa.alpha
+  let final dfa = QS.to_list dfa.final
 
-  let step_table states alpha step  =
+  let step dfa q s =
+    if not (AS.mem s dfa.alpha) then
+      Error InvalidChar
+    else
+      Ok (dfa.step q s)
+
+  let ( let* ) = Result.bind
+
+  let rec step' dfa q str =
+    match str with
+    | []        -> Ok q
+    | s :: str' ->
+      let* p = step dfa q s in
+      step' dfa p str'
+
+  let step_tbl dfa =
     let neighbours q =
-      AS.to_list alpha
-      |> List.map (fun a -> (q, a, step q a))
+      AS.to_list dfa.alpha
+      |> List.map (fun a -> (q, a, dfa.step q a))
     in
 
-    QS.to_list states
+    QS.to_list dfa.states
     |> List.map neighbours
     |> List.concat
 
-  let accepts dfa (str:str) =
+  let accepts dfa str =
     let valid = List.for_all (fun s -> AS.mem s dfa.alpha) str in
     if not valid then
-      Error (InvalidString (str, dfa.alpha))
+      Error InvalidChar
     else
-      let final = step' dfa dfa.init str in
+      let* final = step' dfa dfa.init str in
       Ok (QS.mem final dfa.final)
 
-
-  let mk_dfa states init alpha final step =
-    if QS.is_empty states then
+  let chk_states dfa =
+    if QS.is_empty dfa.states then
       Error EmptyStates
-
-    else if AS.is_empty alpha then
-      Error EmptyAlpha
-
-    else if not (QS.mem init states) then
-      Error (InvalidInit (init, states))
-
-    else if not (QS.subset final states) then
-      Error (InvalidFinal (final, states))
-
     else
-      let table = step_table states alpha step in
+      Ok dfa
 
-      let invalid_trans =
-        List.find_opt (fun (_, _, q) -> not (QS.mem q states)) table
-      in
+  let chk_init dfa =
+    if not (QS.mem dfa.init dfa.states)
+    then Error InvalidInit
+    else Ok dfa
 
-      match invalid_trans with
-      | Some (q1, a, q2) ->
-        Error (InvalidStep (q1, a, q2, states))
-      | None ->
-        Ok {
-          states = states;
-          init   = init  ;
-          alpha  = alpha ;
-          final  = final ;
-          step   = step
-        }
+  let chk_alpha dfa =
+    if AS.is_empty dfa.alpha then
+      Error EmptyAlpha
+    else
+      Ok dfa
+
+  let chk_final dfa =
+    if not (QS.subset dfa.final dfa.states) then
+      Error InvalidFinal
+    else
+      Ok dfa
+
+  let chk_step dfa =
+    let bad =
+      QS.exists
+        (fun q ->
+           AS.exists
+             (fun a -> not (QS.mem (dfa.step q a) dfa.states))
+             dfa.alpha)
+        dfa.states
+    in
+    if bad then
+      Error InvalidStep
+    else Ok dfa
+
+  let chk_dfa dfa =
+    let* dfa = chk_states dfa in
+    let* dfa = chk_init   dfa in
+    let* dfa = chk_alpha  dfa in
+    let* dfa = chk_final  dfa in
+    chk_step dfa
+
+  let mk_dfa ~states ~init ~alpha ~final ~step =
+    chk_dfa
+      { states = QS.of_list states;
+        init   = init;
+        alpha  = AS.of_list alpha;
+        final  = QS.of_list final;
+        step   = step
+      }
 end
