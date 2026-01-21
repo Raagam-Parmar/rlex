cae271bee96493ebb8b600ef181c8fa3c58d6adf
Raagam Parmar <raagamparmar@gmail.com>
2026-01-21 17:43:04 +0530

    Added module lexbuf for holding metadata related to input buffers.

--- lib/engine.ml
+++ lib/engine.ml
@@ -1,7 +1,3 @@
-open Pattern
-open Pattern.Infix
-open Matcher
-
 module Position =
 struct
   type t =
@@ -17,4 +13,39 @@ struct
     ; pos_lnum=0
     ; pos_bol=0
     }
+end
+
+
+module Lexbuf =
+struct
+  type t =
+    { lex_buf : string
+    ; lex_buf_len : int
+    ; mutable lex_start_p : Position.t
+    (** Points to the next character in the buffer to be scanned. *)
+    ; mutable lex_curr_p : Position.t
+    ; mutable eof_reached : bool
+    }
+
+  let lexbuf_of_string str =
+    { lex_buf=str
+    ; lex_buf_len=String.length str
+    ; lex_start_p=Position.zero
+    ; lex_curr_p=Position.zero
+    ; eof_reached=false
+    }
+
+  let next_char lexbuf =
+    let i = lexbuf.lex_curr_p.pos_cnum in
+    if i >= lexbuf.lex_buf_len
+    then None
+    else Some lexbuf.lex_buf.[i]
+
+  let unsafe_next_char lexbuf =
+    let i = lexbuf.lex_curr_p.pos_cnum in
+    lexbuf.lex_buf.[i]
+
+  let advance lexbuf =
+    let i = lexbuf.lex_curr_p.pos_cnum in
+    lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_cnum = i + 1 }
 end
